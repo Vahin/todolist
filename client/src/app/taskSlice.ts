55dae4e1fd0855227337dd73126cdeb773cfdd36
Vahin <danil.e.vakhrushev@gmail.com>
2023-03-10 00:54:30 +0500

    Добавил типизацию createAsyncThunk

--- client/src/app/taskSlice.ts
+++ client/src/app/taskSlice.ts
@@ -1,4 +1,9 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {
+  createAsyncThunk,
+  createSlice,
+  isRejectedWithValue,
+  PayloadAction,
+} from "@reduxjs/toolkit";
 import { tasksApi } from "../api/taskApi";
 
 export type TTask = {
@@ -11,7 +16,7 @@ export type TTask = {
 type InitialState = {
   tasks: TTask[];
   status: "pending" | "fulfilled" | "rejected";
-  error: string | null;
+  error?: string | null;
 };
 
 const initialState: InitialState = {
@@ -20,11 +25,11 @@ const initialState: InitialState = {
   error: null,
 };
 
-type CreateNewTaskType = {
+type CreateNewTaskPayloadType = {
   content: string;
 };
 
-type removeTaskPayloadType = {
+type RemoveTaskPayloadType = {
   id: number;
 };
 
@@ -34,58 +39,62 @@ type toggleTaskStatusPayloadType = {
   completed: boolean;
 };
 
-export const fetchTasks = createAsyncThunk(
-  "tasks/fetchTodo",
-  async (_, { rejectWithValue }) => {
-    try {
-      const result = await tasksApi.getTasks();
+export const fetchTasks = createAsyncThunk<
+  TTask[],
+  undefined,
+  { rejectValue: string }
+>("tasks/fetchTodo", async (_, { rejectWithValue }) => {
+  try {
+    const result = await tasksApi.getTasks();
 
-      return result;
-    } catch (err: any) {
-      return rejectWithValue(err.message);
-    }
+    return result;
+  } catch (err: any) {
+    return rejectWithValue(err.message);
   }
-);
+});
 
-export const createNewTask = createAsyncThunk(
-  "tasks/createNewTask",
-  async ({ content }: CreateNewTaskType, { rejectWithValue }) => {
-    try {
-      const createdAt = new Date().toISOString();
-      const result = await tasksApi.createTask({ content, createdAt });
+export const createNewTask = createAsyncThunk<
+  TTask,
+  CreateNewTaskPayloadType,
+  { rejectValue: string }
+>("tasks/createNewTask", async ({ content }, { rejectWithValue }) => {
+  try {
+    const createdAt = new Date().toISOString();
+    const result = await tasksApi.createTask({ content, createdAt });
 
-      return result;
-    } catch (err: any) {
-      rejectWithValue(err.message);
-    }
+    return result;
+  } catch (err: any) {
+    return rejectWithValue(err.message);
   }
-);
+});
 
-export const removeTask = createAsyncThunk(
-  "tasks/removeTask",
-  ({ id }: removeTaskPayloadType, { rejectWithValue }) => {
-    try {
-      tasksApi.removeTask({ id: String(id) });
+export const removeTask = createAsyncThunk<
+  RemoveTaskPayloadType,
+  RemoveTaskPayloadType,
+  { rejectValue: string }
+>("tasks/removeTask", async ({ id }, { rejectWithValue }) => {
+  try {
+    await tasksApi.removeTask({ id: String(id) });
 
-      return { id };
-    } catch (err: any) {
-      rejectWithValue(err.message);
-    }
+    return { id };
+  } catch (err: any) {
+    return rejectWithValue(err.message);
   }
-);
+});
 
-export const toggleTaskStatus = createAsyncThunk(
-  "task/toggleTaskStatus",
-  async (
-    { id, content, completed }: toggleTaskStatusPayloadType,
-    { rejectWithValue }
-  ) => {
+export const toggleTaskStatus = createAsyncThunk<
+  TTask,
+  toggleTaskStatusPayloadType,
+  { rejectValue: string }
+>(
+  "tasks/toggleTaskStatus",
+  async ({ id, content, completed }, { rejectWithValue }) => {
     try {
       const result = await tasksApi.updateTask({ id, content, completed });
 
       return result;
     } catch (err: any) {
-      rejectWithValue(err.message);
+      return rejectWithValue(err.message);
     }
   }
 );
@@ -95,35 +104,38 @@ const taskSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(fetchTasks.pending, (state) => {
-      state.status = "pending";
-      state.error = null;
-    });
-    builder.addCase(fetchTasks.fulfilled, (state, action) => {
-      state.status = "fulfilled";
-      state.error = null;
-      state.tasks = action.payload;
-    });
-    builder.addCase(fetchTasks.rejected, (state, action: any) => {
-      state.status = "rejected";
-      state.error = action.payload;
-    });
-    builder.addCase(createNewTask.fulfilled, (state, action) => {
-      state.tasks.push(action.payload);
-    });
-    builder.addCase(createNewTask.rejected, (state, action: any) => {
-      state.status = "rejected";
-      state.error = action.payload;
-    });
-    builder.addCase(removeTask.fulfilled, (state, action: any) => {
-      state.tasks = state.tasks.filter((task) => task.id !== action.payload.id);
-    });
-    builder.addCase(toggleTaskStatus.fulfilled, (state, action: any) => {
-      const task = state.tasks.find((task) => task.id === action.payload.id);
-      if (!task) return;
+    builder
+      .addCase(fetchTasks.pending, (state) => {
+        state.status = "pending";
+        state.error = null;
+      })
+      .addCase(fetchTasks.fulfilled, (state, action) => {
+        state.status = "fulfilled";
+        state.error = null;
+        state.tasks = action.payload;
+      })
+      .addCase(fetchTasks.rejected, (state, action) => {
+        state.status = "rejected";
+        state.error = action.payload;
+      })
+      .addCase(createNewTask.fulfilled, (state, action) => {
+        state.tasks.push(action.payload);
+      })
+      .addCase(createNewTask.rejected, (state, action) => {
+        state.status = "rejected";
+        state.error = action.payload;
+      })
+      .addCase(removeTask.fulfilled, (state, action) => {
+        state.tasks = state.tasks.filter(
+          (task) => task.id !== action.payload.id
+        );
+      })
+      .addCase(toggleTaskStatus.fulfilled, (state, action) => {
+        const task = state.tasks.find((task) => task.id === action.payload.id);
+        if (!task) return;
 
-      task.completed = action.payload.completed;
-    });
+        task.completed = action.payload.completed;
+      });
   },
 });
 
